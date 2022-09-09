fa5cff6d65e4d54593e4b23ba2d54a5f990c8125
Wareis <reiskofi@gmail.com>
2022-09-09 04:20:37 +0000

    Commits all man parts

--- lists.h
+++ lists.h
@@ -4,7 +4,7 @@
 #include "monty.h"
 
 /* list_funcs1.c */
-size_t dlistint_len(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 size_t print_dlistint(const dlistint_t *h);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
